b0ee6ca7d485816d621fcefa714718a54794d2ba
Sajeda Akter <sajeda16akter@gmail.com>
2023-12-25 20:09:39 +0600

    'little-change'

--- src/components/Home/HomePageDetails/Banner.jsx
+++ src/components/Home/HomePageDetails/Banner.jsx
@@ -4,37 +4,33 @@ const Banner = () => {
   return (
   <div>
     <div className="hero  bg-[#186F65]  my-5 rounded-md">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <img  src="https://media.istockphoto.com/id/1491352519/photo/traditional-wooden-house-of-the-kalasha-people.webp?b=1&s=170667a&w=0&k=20&c=uqF6oclOQVl76HIHjzOpnVGm7Ff2B4M5o0CMT1wgkDY=" className="lg:w-2/4  rounded-lg " />
+  <div className="hero-content flex  flex-col lg:flex-row-reverse">
+    <img  src="https://media.istockphoto.com/id/1491352519/photo/traditional-wooden-house-of-the-kalasha-people.webp?b=1&s=170667a&w=0&k=20&c=uqF6oclOQVl76HIHjzOpnVGm7Ff2B4M5o0CMT1wgkDY=" className="lg:w-3/4  rounded-lg " />
   
- <div className="flex flex-col lg:gap-16 text-white">
+ <div className="flex flex-col lg:gap-3 justify-between text-white">
  <p className=" text-3xl text-center mt-5 text-white">35% discount for
         <br /><span className="font-extrabold text-white lg:text-5xl">Bijoy Dibosh</span>
       </p>
-      <p className=" text-2xl ml-12 mt-6">Offer Facilities</p>
-      <ul className=" ml-12 px-6">
+      <p className=" text-2xl lg:ml-12  mt-6 lg:mt-0 lg:-mb-4">Offer Facilities</p>
+      <ul className="ml-10 lg:ml-12 ">
         <li>1.Fitness Center</li>
         <li>2.Business Center</li>
         <li>3.Pool side</li>
         <li>4.Free snack </li>
       </ul>
- </div>
- <div className="card-actions w-48">
-                  <button className="btn border-2 ] bg-[#016A70]  text-white text-xl w-48">
+      <div className="card-actions w-48">
+                  <button className="btn border-2  hover:text-black bg-[#016A70]  text-white text-xl w-48">
                     Action
                   </button>
                 </div>
 
+ </div>
+
+
    </div>
     </div>
 
-    {/* <div className="flex gap-5 flex-col-reverse">
-      <div className="flex gap-4">
-      <img className="lg:w-44 lg:h-44 w-24 h-24 rounded-sm" src="https://i.ibb.co/3WJdP76/Ski-Chalet.webp" alt="" />
-      <img className="lg:w-44 lg:h-44 w-24 h-24 rounded-sm"  src="https://i.ibb.co/kGFhV1R/Executive.webp" alt="" />
-      <img className="lg:w-44 lg:h-44 w-24 h-24 rounded-sm lg:me-16"  src="https://media.istockphoto.com/id/1353140785/photo/christmas-celebration-at-cozy-cottage.webp?b=1&s=170667a&w=0&k=20&c=GDqH_gPsB23H6W6bTXIDUIg8t-ugSp8hc1Aqm0gkRzk=" alt="" />
-      </div>
-     </div> */}
+   
   </div>
 
   );
--- src/components/Home/HomePageDetails/TextAnimation.jsx
+++ src/components/Home/HomePageDetails/TextAnimation.jsx
@@ -4,16 +4,16 @@ import { useSpring, animated } from 'react-spring';
 
 const TextAnimation = () => {
     const props = useSpring({
-        from: { transform: 'translateX(-0%)', color: 'orange',fontFamily: 'Arial, sans-serif'},
-        to: { transform: 'translateX(100%)', color: 'purple', fontFamily: 'Times New Roman, serif'  },
+        from: { transform: 'translateX(25%)', color: 'orange',fontFamily: 'Arial, sans-serif'},
+        to: { transform: 'translateX(50%)', color: 'purple', fontFamily: 'Times New Roman, serif'  },
         loop: { reverse: true },
-        config: { duration: 6000 },
+        config: { duration: 5000 },
        
       });
 
    
 
-      return <animated.div style={props} className="text-5xl text-bold my-12">COMFORT HOTEL</animated.div>;
+      return <animated.div style={props} className="lg:text-5xl text-2xl text-bold my-12">COMFORT HOTEL</animated.div>;
 
 };
 
